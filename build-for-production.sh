ad853c6826950b55b09d47484258b1dac7e361d0
Rune Morling <ermo@serpentos.com>
2022-12-15 00:03:24 +0100

    build: Don't hardcode D runtime GC options
    These are better left for the actual invocation during service bringup.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- build-for-production.sh
+++ build-for-production.sh
@@ -12,5 +12,5 @@ set -x
 # remove everything not tracked by git and reset to origin
 git clean -dfx && git reset --hard HEAD
 # Notes re. GC magic here: https://forum.dlang.org/post/befrzndhowlwnvlqcoxx@forum.dlang.org
-DFLAGS="--flto=thin -O3 --static --DRT-gcopt=heapSizeFactor:0.25" dub build -b release --parallel ${*}
+DFLAGS="--flto=thin -O3 --static" dub build -b release --parallel ${*}
 strip website
