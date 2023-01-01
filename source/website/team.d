5802a0ecf6867cd045bebbb4223a051ed8dc1a80
Ikey Doherty <ikey@serpentos.com>
2023-01-01 12:22:56 +0000

    Update copyright ranges
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/website/team.d
+++ source/website/team.d
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
  *
  * SPDX-License-Identifier: Zlib
  */
@@ -11,7 +11,7 @@ module website.team;
  *
  * Handle team rendering / updating
  *
- * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
  * License: Zlib
  */
 
