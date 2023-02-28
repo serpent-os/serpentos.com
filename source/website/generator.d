365811e4e4920bc4351487b193ec99c4debb3003
Ikey Doherty <ikey@serpentos.com>
2023-02-28 22:52:05 +0000

    generator: Add the loading code
    We parallel load all content into memory for sorting as the Post model,
    prior to baking.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/website/generator.d
+++ source/website/generator.d
@@ -16,7 +16,31 @@
 module website.generator;
 
 import std.exception : enforce;
-import std.file : exists, isDir, mkdirRecurse, rmdirRecurse;
+import std.file;
+import website.models;
+import std.path : buildPath, relativePath;
+import std.parallelism : TaskPool;
+import std.array : array;
+import std.algorithm : sort, map;
+import std.string : startsWith;
+import website.markdownpage;
+import vibe.d;
+
+/** 
+ * Private mapping type to solve the dual-context ldc issue
+ */
+private static struct LoadableEntry
+{
+    /** 
+     * Relative to base tree
+     */
+    string relaPath;
+
+    /** 
+     * Real path for the entry
+     */
+    string path;
+}
 
 /** 
  * Generate the refreshed website using the built-in diet
@@ -47,6 +71,9 @@ public final class WebsiteGenerator
             outputDirectory.rmdirRecurse();
             outputDirectory.mkdirRecurse();
         }
+
+        contentPath = inputDirectory.buildPath("content");
+        enforce(contentPath.exists, contentPath ~ " does not exist");
     }
 
     /** 
@@ -56,11 +83,70 @@ public final class WebsiteGenerator
      */
     void build() @safe
     {
-        throw new Exception("not yet implemented");
+        immutable tsStart = Clock.currTime();
+        auto content = loadContent();
+        immutable tsEnd = Clock.currTime();
+        logInfo(format!"Loaded %s pages in %s"(content.length, tsEnd - tsStart));
     }
 
 private:
 
+    /** 
+     * Preload all markdown pages into Post[] slice
+     *
+     * Returns: Array of content
+     */
+    Post[] loadContent() @safe
+    {
+        /* parallel processing */
+        auto tp = new TaskPool();
+        scope (exit)
+        {
+            tp.finish();
+        }
+        tp.isDaemon = false;
+
+        auto inputTree = () @trusted {
+            return contentPath.dirEntries("*.md", SpanMode.depth).map!((string s) {
+                return LoadableEntry(s.relativePath(inputDirectory), s);
+            }).array();
+        }();
+
+        auto pages = () @trusted { return tp.map!loadPage(inputTree).array; }();
+        /* Newest first */
+        pages.sort!"a.tsCreated > b.tsCreated";
+
+        return pages;
+    }
+
+    /** 
+     * Load a markdown page from the given path
+     *
+     * Params:
+     *   postPath = Where we found the post
+     * Returns: A Post model struct
+     */
+    static Post loadPage(LoadableEntry entry) @safe
+    {
+        auto page = new MarkdownPage();
+        page.loadFile(entry.path);
+
+        /* Convert/copy into model */
+        Post model;
+        model.type = entry.relaPath.startsWith("content/blog") ? PostType.RegularPost
+            : PostType.Page;
+        model.author = page.author;
+        model.featuredImage = page.featuredImage;
+        model.processedContent = page.content;
+        model.processedSummary = page.summary;
+        model.slug = page.slug;
+        model.title = page.title;
+        model.tsCreated = page.date.toUTC.toUnixTime;
+        model.tsModified = model.tsCreated;
+        return model;
+    }
+
     string inputDirectory;
     string outputDirectory;
+    string contentPath;
 }
