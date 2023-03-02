6ea3bd517f7d2b4f9a58e83dc7d24a8b94e3f822
Ikey Doherty <ikey@serpentos.com>
2023-03-02 09:25:35 +0000

    main: Add a new `--serve` mode for built-in web server
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/main.d
+++ source/main.d
@@ -17,6 +17,7 @@ module main;
 
 import vibe.d;
 import website.generator;
+import std.algorithm : canFind;
 
 /** 
  * Main entry for the binary
@@ -28,4 +29,23 @@ void main(string[] args) @safe
 {
     auto gen = new WebsiteGenerator(".", "output");
     gen.build();
+
+    if (!args.canFind("--serve"))
+    {
+        return;
+    }
+
+    /* Serve generated content via HTTP due to file:// security policy */
+    auto settings = new HTTPServerSettings();
+    settings.port = 8080;
+    settings.useCompressionIfPossible = true;
+    auto router = new URLRouter();
+    auto fileSettings = new HTTPFileServerSettings();
+    fileSettings.maxAge = 30.days;
+    fileSettings.serverPathPrefix = "/";
+    fileSettings.options = HTTPFileServerOption.failIfNotFound;
+    auto fileHandler = serveStaticFiles("output");
+    router.get("*", fileHandler);
+    listenHTTP(settings, router);
+    runApplication();
 }
